6d392d94d92ba20e06da69394fbddc8275fa08b5
moamenelioa-jpg <moamenelioa@gmail.com>
2026-03-18 16:58:44 +0200

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,13 @@
+console.log("Site is working 🚀");
+window.addEventListener("scroll", () => {
+    const cards = document.querySelectorAll(".skill-card");
+
+    cards.forEach(card => {
+        const top = card.getBoundingClientRect().top;
+
+        if (top < window.innerHeight - 100) {
+            card.style.opacity = "1";
+            card.style.transform = "translateY(0)";
+        }
+    });
+});
